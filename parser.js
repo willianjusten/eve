783749f728314eb98332c39644c0a8d69563af2f
Willian Justen <willianjustenqui@gmail.com>
2016-04-18 15:27:49 -0300

    Fix settings require

--- parser.js
+++ parser.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const settings = require('./settings.js');
+
 exports.getFirstMessagingEntry = (body) => {
   const val = body.object == 'page' &&
     body.entry &&
     Array.isArray(body.entry) &&
     body.entry.length > 0 &&
     body.entry[0] &&
-    body.entry[0].id == FB_PAGE_ID &&
+    body.entry[0].id == settings.FB_PAGE_ID &&
     body.entry[0].messaging &&
     Array.isArray(body.entry[0].messaging) &&
     body.entry[0].messaging.length > 0 &&
